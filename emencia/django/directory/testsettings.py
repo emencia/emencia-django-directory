22dfe39cbf56b654f99f491d7614c110469301f4
Fantomas42 <fantomas42@gmail.com>
2010-02-16 11:16:20 +0100

    preparing test settings

--- emencia/django/directory/testsettings.py
+++ emencia/django/directory/testsettings.py
@@ -5,3 +5,18 @@ DATABASE_NAME = '/tmp/directory.db'
 INSTALLED_APPS = ['emencia.django.directory',]
 #ROOT_URLCONF = 'emencia.django.directory.urls'
 
+LANGUAGE_CODE = 'fr'
+
+LANGUAGES = (
+    ('fr', 'French'),
+    ('en', 'English'),
+    )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    )
+
